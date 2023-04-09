4e97a1cd55b2a9804c7895a3c2f869390b9516ec
kushagra <kushagra111awasthi@gmail.com>
2023-04-09 17:27:15 +0530

    Signupone

---
+++ Bank_Management_System/signup1.java
@@ -0,0 +1,15 @@
+package Bank_Management_System;
+import javax.swing.*;
+import java.awt.*;
+public class signup1 extends JFrame {
+
+    signup1(){
+        setSize(850,800);
+        setLocation(350,10);
+        setVisible(true);
+
+    }
+    public static void main(String args[]){
+        new signup1();
+    }
+}
