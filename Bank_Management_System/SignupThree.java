2499eb4fa94c2c74e607316872a01d6c4aacc50a
kushagra <kushagra111awasthi@gmail.com>
2023-04-15 18:31:03 +0530

    progress

--- Bank_Management_System/SignupThree.java
+++ Bank_Management_System/SignupThree.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class SignupThree extends JFrame{
+    JRadioButton r1,r2,r3,r4;
     SignupThree(){
 
         setLayout(null);
@@ -11,6 +12,76 @@ public class SignupThree extends JFrame{
         l1.setFont(new Font("Raleway",Font.BOLD,22));
         l1.setBounds(280,40,400,40);
         add(l1);
+
+        JLabel type =new JLabel("Account type:");
+        type.setFont(new Font("Raleway",Font.BOLD,22));
+        type.setBounds(100,140,200,30);
+        add(type);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Raleway",Font.BOLD,16));
+        setBackground(Color.WHITE);
+        r1.setBounds(100,180,250,20);
+        add(r1);
+
+        r2 = new JRadioButton("Fixed Deposite Account");
+        r2.setFont(new Font("Raleway",Font.BOLD,16));
+        setBackground(Color.WHITE);
+        r2.setBounds(350,180,250,20);
+        add(r2);
+
+        r3 = new JRadioButton("Current Account");
+        r3.setFont(new Font("Raleway",Font.BOLD,16));
+        setBackground(Color.WHITE);
+        r3.setBounds(100,220,250,20);
+        add(r3);
+
+        r4 = new JRadioButton("Recurring Deposite Account");
+        r4.setFont(new Font("Raleway",Font.BOLD,16));
+        setBackground(Color.WHITE);
+        r4.setBounds(350,220,250,20);
+        add(r4);
+
+        ButtonGroup groupaccount = new ButtonGroup();
+        groupaccount.add(r1);
+        groupaccount.add(r2);
+        groupaccount.add(r3);
+        groupaccount.add(r4);
+
+        JLabel card =new JLabel("Card Number:");
+        card.setFont(new Font("Raleway",Font.BOLD,22));
+        card.setBounds(100,300,200,30);
+        add(card);
+
+        JLabel number =new JLabel("XXXX-XXXX-XXXX-0702");
+        number.setFont(new Font("Raleway",Font.BOLD,22));
+        number.setBounds(330,300,300,30);
+        add(number);
+
+        JLabel carddetails =new JLabel("Your 16 digit card number");
+        carddetails.setFont(new Font("Raleway",Font.BOLD,12));
+        carddetails.setBounds(100,330,300,20);
+        add(carddetails);
+
+        JLabel pin =new JLabel("PIN:");
+        pin.setFont(new Font("Raleway",Font.BOLD,22));
+        pin.setBounds(100,370,200,30);
+        add(pin);
+
+        JLabel pnumber =new JLabel("XXXX");
+        pnumber.setFont(new Font("Raleway",Font.BOLD,22));
+        pnumber.setBounds(330,370,300,30);
+        add(pnumber);
+
+        JLabel pindetails =new JLabel("Your 4 digit pin number");
+        pindetails.setFont(new Font("Raleway",Font.BOLD,12));
+        pindetails.setBounds(100,400,300,20);
+        add(pindetails);
+
+        JLabel services =new JLabel("Services required  :");
+        services.setFont(new Font("Raleway",Font.BOLD,22));
+        services.setBounds(100,450,200,30);
+        add(services);
 
         setSize(850,820);
         setLocation(350,0);
