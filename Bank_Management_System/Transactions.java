1f53bd775b899d1f4b8076fcf95368d104d524bd
kushagra <kushagra111awasthi@gmail.com>
2023-04-16 17:26:55 +0530

    Transaction

--- Bank_Management_System/Transactions.java
+++ Bank_Management_System/Transactions.java
@@ -2,18 +2,70 @@ package Bank_Management_System;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Transactions extends JFrame {
+public class Transactions extends JFrame implements ActionListener {
+    JButton deposite , withdrawl ,fastcash,ministatement,pinchange,balanceenquiry,exit;
     Transactions(){
 
         ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("Bank_Management_System/icons/atm.jpg"));
         Image i2 = i1.getImage().getScaledInstance(900,900,Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel image =new JLabel(i3);
+        image.setBounds(0,0,900,900);
         add(image);
+
+        JLabel text = new JLabel("Please Select your transaction");
+        text.setBounds(210,300,700,35);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("Syestem",Font.BOLD,16));
+        image.add(text);
+
+        deposite = new JButton("Deposite");
+        deposite.setBounds(170,415,150,30);
+        deposite.addActionListener(this);
+        image.add(deposite);
+
+        withdrawl = new JButton("Cash Withdrawl");
+        withdrawl.setBounds(355,415,150,30);
+        withdrawl.addActionListener(this);
+        image.add(withdrawl);
+
+        fastcash = new JButton("Fast Cash");
+        fastcash.setBounds(170,450,150,30);
+        fastcash.addActionListener(this);
+        image.add(fastcash);
+
+        ministatement = new JButton("Mini Statement");
+        ministatement.setBounds(355,450,150,30);
+        ministatement.addActionListener(this);
+        image.add(ministatement);
+
+        pinchange = new JButton("Pin Change");
+        pinchange.setBounds(170,485,150,30);
+        pinchange.addActionListener(this);
+        image.add(pinchange);
+
+        balanceenquiry = new JButton("Balance Enquiry");
+        balanceenquiry.setBounds(355,485,150,30);
+        balanceenquiry.addActionListener(this);
+        image.add(balanceenquiry);
+
+        exit = new JButton("Exit");
+        exit.setBounds(355,520,150,30);
+        exit.addActionListener(this);
+        image.add(exit);
+
         setSize(900,900);
         setLocation(300,0);
+        setUndecorated(true);
         setVisible(true );
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource()==exit){
+            System.exit(0);
+        }
     }
 
     public static void main(String args[]){
